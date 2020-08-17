4308673a69938f7a5a3775c54e55b9667ac0fcd2
Rafael Lopez <lpz.raf@gmail.com>
2020-08-16 21:28:42 -0400

    It will add create a user and persist session data

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import (Flask, render_template, request, abort, 
-                    redirect, url_for, jsonify,session,g)
+                    redirect, url_for, jsonify,session,g, session)
 from flask_modus import Modus
-from model import db, save_db
+from user import User
+from model import db, save_db, user_db, save_user_db
 import datetime
+
+from random import seed
+from random import random
 
 
 app = Flask(__name__)
@@ -10,15 +14,6 @@ app.secret_key = 'supermegadupersecretkey'
 modus = Modus(app)
 
 date = datetime.datetime.now().strftime('%A, %b %d, %Y')
-
-# session
-@app.before_request
-def before_request():
-    g.user = None
-
-    if 'user_id' in session:
-        user = [x for x in users if x.id == session['user_id']][0]
-        g.user = user
 
 # notes homepage
 @app.route('/notes')
@@ -97,14 +92,25 @@ def remove_note(index):
     except IndexError:
         abort(404)
 
-class User:
-    def __init__(self,id,username,password):
-        self.id = id
-        self.username = username
-        self.password = password
+# session
+@app.before_request
+def before_request():
+    g.user = None
 
-    def __repr__(self):
-        return f'<User: {self.username}>'
+    if 'user_id' in session:
+        global user_db
+        # user = [x for x in user_db if x['id'] == session['user_id']][0]
+        user = [x for x in user_db if x['id'] == session['user_id']][0]
+        g.user = user
+
+# class User:
+#     def __init__(self,id,username,password):
+#         self.id = id
+#         self.username = username
+#         self.password = password
+
+#     def __repr__(self):
+#         return f'<User: {self.username}>'
 
 users = []
 users.append(User(id=1, username='Rafa', password='password'))
@@ -130,6 +136,16 @@ def login():
 
     return render_template('login.html')
 
+# logout
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    global date
+    if request.method == 'POST':
+        session.pop('user_id', None)
+        return redirect(url_for('login'))
+    else:
+        return render_template('logout.html', date=date)
+
 # profile
 @app.route('/profile')
 def profile():
@@ -137,3 +153,34 @@ def profile():
         return redirect(url_for('login'))
 
     return render_template('profile.html')
+
+# new 
+# creating a user
+@app.route('/users/new', methods=["GET", "POST"])
+def add_user():
+    global date
+    if request.method == "POST":
+        seed(1)
+        user = {"id": random(),
+                "username": request.form['username'],  
+                "password": request.form['password']}
+        session['user_id'] = user['id']
+        user_db.append(user)
+        save_user_db()
+        return redirect(url_for("profile"))
+    
+    return render_template("add_user.html", date=date)
+
+#protected
+@app.route('/protected')
+def protected():
+    if g.user:
+        return render_template('protected.html')
+    return redirect(url_for('login'))
+
+# drop session
+@app.route('/dropsession')
+def dropsession():
+    session.pop('user_id', None)
+    return 'Dropped!'
+
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import json
 
+# notes db
 def load_db():
     with open("notes_data_db.json") as f:
         return json.load(f)
@@ -8,4 +9,15 @@ def save_db():
     with open("notes_data_db.json", 'w') as f:
         return json.dump(db, f)
 
+# user db
+def load_user_db():
+    with open("users_data_db.json") as f:
+        return json.load(f)
+
+def save_user_db():
+    with open("users_data_db.json", 'w') as f:
+        return json.dump(user_db, f)
+
+
 db = load_db()
+user_db = load_user_db()
